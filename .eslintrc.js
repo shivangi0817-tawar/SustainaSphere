8e6ab8e5746ad12b8b417d7be8bf8122c8578759
Shivangi Tawar <shivangitawar@gmail.com>
2025-04-24 21:32:37 +0530

    Add files via upload

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true,
+  extends: [
+    'react-app',
+    'react-app/jest'
+  ],
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  rules: {
+    // Add any custom rules here
+  }
+}; 
